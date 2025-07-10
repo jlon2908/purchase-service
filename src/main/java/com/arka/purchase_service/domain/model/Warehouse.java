faa983c065e586ba697942b2bb40eab1439fd357
Jonathan Londoño <jonathanlon.66@gmail.com>
2025-07-09 23:20:45 -0500

    firs github actions

--- src/main/java/com/arka/purchase_service/domain/model/Warehouse.java
+++ src/main/java/com/arka/purchase_service/domain/model/Warehouse.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Warehouse {
+    
     private String code;
     private String name;
     private double latitude;
